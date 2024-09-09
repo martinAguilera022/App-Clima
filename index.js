f1f057e38133f1f40ea2f2d2b2763d072ee91b4e
martin aguilera <martin.aguilera0233@gmail.com>
2024-09-09 20:52:43 -0300

    App clima

---
+++ index.js
@@ -0,0 +1,87 @@
+
+const APP_ID = '4090239d69cdb3874de692fd18539299';
+
+
+window.addEventListener('load',()=>{
+    let long;
+    let lat;
+
+    let ubicacion = document.getElementById('ubicacion');
+    let grados = document.getElementById('grados');
+    let iconoAnimado  = document.getElementById('icon-animado')
+    let temperaturaDescrip = document.getElementById('temperatura-descrip') 
+    let sensacionTermica = document.getElementById('sensacion-termica')
+   
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition( position =>{
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+        
+
+            const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&lang=es&units=metric&appid=${APP_ID}`
+
+            fetch(url)
+                .then ( response => { return response.json () })
+                .then ( data => {
+                    console.log (data)
+
+                   
+                    // let iconCode = data.weather[0].icon
+    	            // let urlIcon = `https://openweathermap.org/img/wn/${iconCode}.png`
+                    // console.log(urlIcon)
+                    let pais = (data.sys.country)
+                    let ciudad = (data.name)
+
+                    
+                    ubicacion.textContent = (`${ciudad} , ${pais}`)
+                  
+                    let temp = Math.round(data.main.temp)
+                    grados.textContent = `${temp} °C`
+                    sensacionTermica.textContent = Math.round(data.main.feels_like)+" °C "
+                    temperaturaDescrip.textContent =(data.weather[0].description).toUpperCase()
+
+
+                    // iconos 
+
+                    console.log(data.weather[0].main)
+                    switch (data.weather[0].main) {
+                        case 'Thunderstorm':
+                          iconoAnimado.src='animated/thunder.svg'
+                          console.log('TORMENTA');
+                          break;
+                        case 'Drizzle':
+                          iconoAnimado.src='animated/rainy-2.svg'
+                          console.log('LLOVIZNA');
+                          break;
+                        case 'Rain':
+                          iconoAnimado.src='animated/rainy-7.svg'
+                          console.log('LLUVIA');
+                          break;
+                        case 'Snow':
+                          iconoAnimado.src='animated/snowy-6.svg'
+                            console.log('NIEVE');
+                          break;                        
+                        case 'Clear':
+                            iconoAnimado.src='animated/day.svg'
+                            console.log('LIMPIO');
+                          break;
+                        case 'Atmosphere':
+                            iconoAnimado.src='animated/weather.svg'
+                            console.log('ATMOSFERA');
+                            break;  
+                        case 'Clouds':
+                            iconoAnimado.src='animated/cloudy.svg'
+                            console.log('NUBES');
+                            break;  
+                        default:
+                          iconoAnimado.src='animated/cloudy-day-1.svg'
+                          console.log('por defecto');
+                      }
+                })
+                .catch ( error =>{
+                    console.log(error)
+                })
+        
+        })
+    }
+})
